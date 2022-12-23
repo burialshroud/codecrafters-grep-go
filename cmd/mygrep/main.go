5861527b11ce3fe59c1571d74861efa1b2d7b553
burialshroud <burialshroud@proton.me>
2022-12-23 16:26:03 -0700

    lmoji

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func eprintf(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format, a...);
+	fmt.Fprintf(os.Stderr, format, a...)
 }
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
@@ -25,33 +25,36 @@ func main() {
 
 	input := string(input_bytes)
 	pattern := os.Args[2]
-	if len(pattern) == 1 { 
+	if len(pattern) == 1 {
 		if strings.IndexByte(input, pattern[0]) != -1 {
 			os.Exit(0)
 		}
-	} else if len(pattern) == 2 && 
-	pattern[0] == '\\' && pattern[1] == 'd' {
+	} else if len(pattern) == 2 &&
+		pattern[0] == '\\' && pattern[1] == 'd' {
 		if strings.IndexAny(input, "0123456789") != -1 {
 			os.Exit(0)
 		}
 	} else if len(pattern) == 2 &&
-	pattern[0] == '\\' && pattern[1] == 'w' {
-		for i := range(input) {
+		pattern[0] == '\\' && pattern[1] == 'w' {
+		for i := range input {
 			ch := input[i]
 			if (ch >= 'A' && ch <= 'Z') ||
-			(ch >= 'a' && ch <= 'z') ||
-			(ch >= '0' && ch <= '9') ||
-			(ch == '_') {
+				(ch >= 'a' && ch <= 'z') ||
+				(ch >= '0' && ch <= '9') ||
+				(ch == '_') {
 				os.Exit(0)
 			}
 		}
-	
+
 	} else if len(pattern) > 3 &&
-	pattern[0] == '[' && pattern[len(pattern)-1] == ']' {
+		pattern[0] == '[' && pattern[len(pattern)-1] == ']' {
 		if pattern[0] == '^' {
 			reject := pattern[2:len(pattern)-1]
-			if strings.IndexAnyNot(input, accept) != -1 {
-				os.Exit(0)
+			for i := range input {
+				ch := input[i]
+				if !strings.ContainsRune(reject, rune(ch)) {
+					os.Exit(0)
+				}
 			}
 		} else {
 			accept := pattern[1:len(pattern)-1]
